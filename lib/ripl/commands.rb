77d6a47c68368bab130b762591b634c599c53797
Gabriel Horner <gabriel.horner@gmail.com>
2010-11-18 09:15:52 -0500

    initial commands

---
+++ lib/ripl/commands.rb
@@ -0,0 +1,38 @@
+require 'ripl'
+
+module Ripl
+  module Commands
+    VERSION = '0.1.0'
+
+    module Core
+      def self.jumps
+        @jumps ||= [Ripl.shell.loop_eval("self")]
+      end
+
+      def list
+        Ripl::Commands.instance_methods
+      end
+
+      def config(name, val)
+        Ripl.shell.respond_to?("#{name}=") ? Ripl.shell.send("#{name}=", val) :
+          Ripl.config[name] = val
+      end
+
+      def jump(obj)
+        obj = Core.jumps[obj] || obj if obj.is_a?(Fixnum)
+        if !Core.jumps.include?(obj)
+          Core.jumps << obj
+          Ripl.shell.add_commands(obj)
+        end
+        Ripl.shell.binding = obj.send(:binding)
+        obj
+      end
+
+      def jumps
+        Core.jumps
+      end
+    end
+  end
+end
+
+Ripl::Commands.send :include, Ripl::Commands::Core
